d31e864c5bbeb7e66f70e5f55f8144004f99f266
Thiago Giacometi <thiagogiacometi@hotmail.com>
2022-05-08 20:43:59 -0300

    cadastro servico

--- class/servico.js
+++ class/servico.js
@@ -1,8 +1,8 @@
 export default class Servico {
   constructor(nome, descricao, preco, id_lava_rapido) {
-    this.nome = nome
-    this.descricao = descricao
-    this.preco = preco
-    this.id_lava_rapido = id_lava_rapido
+    this.ds_nome_servico = nome;
+    this.ds_servico = descricao;
+    this.ds_preco = preco;
+    this.tbLavaRapidoIdLavaRapido = id_lava_rapido;
   }
 }
--- src/admServiço.js
+++ src/admServiço.js
@@ -1,18 +1,102 @@
-import Servico from '../class/servico.js'
+import Servico from "../class/servico.js";
 
-let lavaRapido = document.getElementById('lavaRapido')
-let nome = document.getElementById('servico')
-let money = document.getElementById('money')
-let descricao = document.getElementById('descricao')
-let btnCadastro = document.getElementById('cadastro')
+let nome = document.getElementById("servico");
+let money = document.getElementById("preco");
+let descricao = document.getElementById("descricao");
+let btnCadastro = document.getElementById("cadastro");
+let table = document.getElementById("table");
+let lavaRapido = document.getElementById("lavaRapidos");
 
-btnCadastro.addEventListener('click', function() {
-  
-  if(servico.value != '' && money.value != '' && descricao.value != ''){ 
-    let servico = new Servico(nome.value, descricao.value , money.value, lavaRapido.value)
-    console.log(servico)
+async function deletarServico(id_servico) {
+  try {
+    await axios.delete(`http://localhost:8090/servicos/${id_servico}`);
+
+    alert("Lava Rápido deletado com sucesso");
+
+    popularTabela();
+  } catch (err) {
+    console.log(err);
   }
-  else{
-    alert("Favor preencher todos os campos")
+}
+
+async function enviarFormulario(event) {
+  console.log(event);
+  if (
+    servico.value != "" &&
+    money.value != "" &&
+    descricao.value != "" &&
+    lavaRapido.value != "null"
+  ) {
+    let servico = new Servico(
+      nome.value,
+      descricao.value,
+      money.value,
+      lavaRapido.value
+    );
+    console.log(servico);
+
+    await axios.post("http://localhost:8090/servicos", servico);
+
+    alert("Lava Rápido cadastrado com sucesso");
+    popularTabela();
+  } else {
+    alert("Favor preencher todos os campos");
   }
-})
+}
+
+async function popularSelect() {
+  try {
+    const dados = await axios.get("http://localhost:8090/lavaRapido");
+    console.log(dados);
+
+    const options = dados.data.forEach((item) => {
+      const option = new Option(item.ds_nome, item.id_lava_rapido);
+      lavaRapido.appendChild(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function popularTabela() {
+  table.innerHTML = "";
+  try {
+    const dados = await axios.get("http://localhost:8090/servicos");
+    console.log(dados);
+
+    dados.data.forEach((item) => {
+      let linha = table.insertRow();
+
+      let nomeServico = linha.insertCell(0);
+      nomeServico.innerHTML = item.ds_nome_servico;
+
+      let preco = linha.insertCell(1);
+      preco.innerHTML = item.ds_preco;
+
+      let lavaRapido = linha.insertCell(2);
+      lavaRapido.innerHTML = item.tb_lava_rapido.ds_nome;
+
+      let descricao = linha.insertCell(3);
+      descricao.innerHTML = item.ds_servico;
+
+      let editar = linha.insertCell(4);
+      editar.innerHTML = `<button  class='button is-info is-small'>Editar</button>`;
+
+      let excluir = linha.insertCell(5);
+      excluir.innerHTML = `<button value="${item.id_servico}" class='button is-danger is-small deletar'>Excluir</button>`;
+    });
+
+    document.querySelectorAll(".deletar").forEach((button) => {
+      button.addEventListener("click", (event) =>
+        deletarServico(event.target.value)
+      );
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+btnCadastro.addEventListener("click", enviarFormulario);
+
+popularTabela();
+popularSelect();
