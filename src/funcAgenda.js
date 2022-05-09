d71e4bba6acf68075d7fa3415746a75a22359b68
Thiago Giacometi <thiagogiacometi@hotmail.com>
2022-05-09 19:34:32 -0300

    tela funcionario

---
+++ src/funcAgenda.js
@@ -0,0 +1,44 @@
+let table = document.getElementById("table");
+
+async function popularTabela() {
+  table.innerHTML = "";
+  try {
+    const dados = await axios.get("http://localhost:8090/agenda");
+    console.log(dados);
+
+    dados.data.forEach((item) => {
+      let linha = table.insertRow();
+
+      let lavaRapido = linha.insertCell(0);
+      lavaRapido.innerHTML = item.tb_lava_rapido.ds_nome;
+
+      let cliente = linha.insertCell(1);
+      cliente.innerHTML = item.tb_cliente.ds_nome;
+
+      let carro = linha.insertCell(2);
+      carro.innerHTML = item.ds_modelo;
+
+      let placa = linha.insertCell(3);
+      placa.innerHTML = item.ds_placa;
+
+      let data = linha.insertCell(4);
+      data.innerHTML = item.dt_agendamento;
+
+      let horario = linha.insertCell(5);
+      horario.innerHTML = item.hr_agendamento;
+
+      let servico = linha.insertCell(6);
+      servico.innerHTML = item.tb_servico.ds_nome_servico;
+
+      let preco = linha.insertCell(7);
+      preco.innerHTML = item.tb_servico.ds_preco;
+
+      let modo = linha.insertCell(8);
+      modo.innerHTML = item.modo;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+popularTabela();
